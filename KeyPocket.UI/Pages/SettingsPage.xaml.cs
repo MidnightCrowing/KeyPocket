50f675435c79d60080e8a7e60866fefe1561e444
HanRabbit <tianzihan5357@163.com>
2026-01-23 20:31:29 +0800

    feat: Remove refresh button for exchange rate and allow direct editing in settings

--- KeyPocket.UI/Pages/SettingsPage.xaml.cs
+++ KeyPocket.UI/Pages/SettingsPage.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 using KeyPocket.UI.ViewModels;
-using System;
-using System.Diagnostics;
 
 namespace KeyPocket.UI.Pages;
 
@@ -13,25 +11,5 @@ public sealed partial class SettingsPage : Page
     {
         ViewModel = new SettingsViewModel();
         InitializeComponent();
-    }
-
-    private async void RefreshRateButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-    {
-        try
-        {
-            Debug.WriteLine("RefreshRateButton_Click: invoking ViewModel.RefreshExchangeRateCommand");
-            if (ViewModel.RefreshExchangeRateCommand.CanExecute(null))
-            {
-                await ViewModel.RefreshExchangeRateCommand.ExecuteAsync(null);
-            }
-            else
-            {
-                Debug.WriteLine("RefreshRateButton_Click: command cannot execute");
-            }
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine($"RefreshRateButton_Click: exception {ex}");
-        }
     }
 }
